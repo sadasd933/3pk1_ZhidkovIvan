189c349bda31b8f7ea58d089f88daf3b55525b44
qnexc <74954462+sadasd933@users.noreply.github.com>
2023-04-19 12:51:02 +0500

    pz8

---
+++ PZ_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PZ_8
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _student = ContactDataFactory.CreateContactData("Students") as Students;
+            _student.Name = "Petrov Ivan";
+            _student.PhoneNumber = "345863986534";
+            _student.Email = "sdfsdf@gmail.com";
+
+            var _spec = ContactDataFactory.CreateContactData("Specialization") as Specialization;
+            _spec.Name = "Ivan Nekrovot";
+            _spec.PhoneNumber = "6456456456546";
+            _spec.Email = "gdfgdfg@mail.com";
+
+            var _college = ContactDataFactory.CreateContactData("College") as College;
+            _college.Name = "sharaga";
+            _college.PhoneNumber = "323-675-124";
+            _college.Address = "Chkalova, 11";
+
+            var AllContacts = new List<IContact> { _student, _spec, _college };
+
+            foreach (var contact in AllContacts)
+            {
+                Console.WriteLine("\nName: " + contact.Name);
+                Console.WriteLine("Phone number: " + contact.PhoneNumber);
+            }
+        }
+    }
+}
